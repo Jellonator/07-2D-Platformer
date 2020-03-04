a43d76dba196a79c0dae4b7e3de854aecdd21b3e
Jocelyn Beedie <jellonator00@gmail.com>
2020-03-04 16:43:14 -0500

    Button can now affect moving platform

--- Object/MovingPlatform/Platform.gd
+++ Object/MovingPlatform/Platform.gd
@@ -1,21 +1,25 @@
 tool
 extends Node2D
 
+enum ButtonMode{IGNORE, ACTIVATE, DEACTIVATE, FORWARD}
+
 export var speed := 16.0
 export(int) var initial_node := 0 setget set_initial_node
 export(float, 0.0, 1.0) var initial_position := 0.0 setget set_initial_position
 export(int, "Forward", "Backward") var initial_direction := 0
+export(ButtonMode) var button_mode := ButtonMode.IGNORE
 onready var path := get_parent() as Path2D
 onready var curve := path.curve
 var dir := 1
 var cpoint := 0
 var nextpoint := 1
 var timer := 0.0
+var button_status := 0
 
 func editor_update_position():
 	if not Engine.editor_hint or get_parent() == null:
 		return
-	self.position = get_parent().curve.interpolate(initial_node, sineInOut(initial_position))
+	self.position = get_parent().curve.interpolate(initial_node, initial_position)
 
 func set_initial_node(id: int):
 	if Engine.editor_hint and get_parent() != null:
@@ -36,7 +40,6 @@ func _ready():
 		initial_node = int(clamp(initial_node, 0, n-1))
 		editor_update_position()
 		return
-	prints(initial_node, initial_position)
 	if initial_node == 0 and initial_position <= 0.0:
 		cpoint = 0
 		dir = 1
@@ -58,61 +61,94 @@ func _ready():
 			cpoint = initial_node + 1
 			nextpoint = initial_node
 			timer = 1.0-initial_position
-	prints(cpoint, nextpoint, dir, timer)
 
-func cubicInOut(t: float) -> float:
-	t *= 2.0
-	if t <= 1:
-		return (t * t * t) / 2.0
-	else:
-		t -= 2.0
-		return (t * t * t + 2.0) / 2.0
+#func cubicInOut(t: float) -> float:
+#	t *= 2.0
+#	if t <= 1:
+#		return (t * t * t) / 2.0
+#	else:
+#		t -= 2.0
+#		return (t * t * t + 2.0) / 2.0
+#
+#func expInOut(t: float) -> float:
+#	t *= 2.0
+#	if t <= 1:
+#		return pow(2, 10 * t - 10) / 2.0
+#	else:
+#		return (2.0 - pow(2, 10 - 10 * t)) / 2.0
+#
+#func sineInOut(t: float) -> float:
+#	return (1.0 - cos(PI * t)) / 2.0;
 
-func expInOut(t: float) -> float:
-	t *= 2.0
-	if t <= 1:
-		return pow(2, 10 * t - 10) / 2.0
-	else:
-		return (2.0 - pow(2, 10 - 10 * t)) / 2.0
-
-func sineInOut(t: float) -> float:
-	return (1.0 - cos(PI * t)) / 2.0;
+func swap_nodes():
+	var tmp := cpoint
+	cpoint = nextpoint
+	nextpoint = tmp
+	timer = 1.0 - timer
 
 func _physics_process(delta):
 	if Engine.editor_hint:
 		return
 	var frompos := curve.get_point_position(cpoint)
 	var topos := curve.get_point_position(nextpoint)
-	timer += delta * speed / topos.distance_to(frompos)
+	var active := true
+	var do_reverse := true
+	match button_mode:
+		ButtonMode.IGNORE:
+			active = true
+		ButtonMode.ACTIVATE:
+			active = button_status > 0
+		ButtonMode.DEACTIVATE:
+			active = button_status <= 0
+		ButtonMode.FORWARD:
+			active = true
+			do_reverse = false
+			if button_status > 0:
+				if dir == -1:
+					dir = 1
+					swap_nodes()
+			else:
+				if dir == 1:
+					dir = -1
+					swap_nodes()
+	if active:
+		timer += delta * speed / topos.distance_to(frompos)
 	if timer > 1.0:
-		timer = 0.0
 		if dir == 1:
 			if nextpoint+1 >= curve.get_point_count():
-				dir = -1
-				cpoint = nextpoint
-				nextpoint = cpoint - 1
+				if do_reverse:
+					dir = -1
+					cpoint = nextpoint
+					nextpoint = cpoint - 1
+				else:
+					timer = 1.0
 			else:
 				nextpoint += 1
 				cpoint += 1
+				timer = 0.0
 		else:
 			if nextpoint-1 < 0:
-				dir = 1
-				nextpoint = 1
-				cpoint = 0
+				if do_reverse:
+					dir = 1
+					nextpoint = 1
+					cpoint = 0
+				else:
+					timer = 1.0
 			else:
 				nextpoint -= 1
 				cpoint -= 1
+				timer = 0.0
 	var pos: Vector2
 	if dir == 1:
-		pos = curve.interpolate(cpoint, sineInOut(timer))
+		pos = curve.interpolate(cpoint, timer)
 	else:
-		pos = curve.interpolate(nextpoint, sineInOut(1.0-timer))
+		pos = curve.interpolate(nextpoint, 1.0-timer)
 	var ppos = global_position
 	position = pos
 	$Polygon2D.global_position = ppos
 
 func do_press():
-	print("activate")
+	button_status += 1
 
 func do_release():
-	print("deactivate")
+	button_status -= 1

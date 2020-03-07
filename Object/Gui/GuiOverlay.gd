a2e347176d63c4470f8e67880c414e92105b9ad0
Jocelyn Beedie <jellonator00@gmail.com>
2020-03-07 02:58:03 -0500

    Film overlay now goes on and off screen

--- Object/Gui/GuiOverlay.gd
+++ Object/Gui/GuiOverlay.gd
@@ -7,6 +7,7 @@ onready var node_films := $Films
 var show_film_timer := 0.0
 
 func _ready():
+	show_film_timer = 1.0
 	yield(get_tree(), "idle_frame")
 	for film in get_tree().get_nodes_in_group("film"):
 		var id = film.level_id
@@ -24,3 +25,11 @@ func _ready():
 
 func collect_film(id: int):
 	films[id].collect()
+	show_film_timer = 1.0
+
+func _physics_process(delta):
+	show_film_timer -= delta * 0.25
+	if show_film_timer > 0.0:
+		node_films.rect_position.y = clamp(node_films.rect_position.y + delta * 64.0, -16.0, 0.0)
+	else:
+		node_films.rect_position.y = clamp(node_films.rect_position.y - delta * 64.0, -16.0, 0.0)

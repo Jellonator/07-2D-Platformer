0a121f9251297dec1ac04ee945284f69e3838e37
Jocelyn Beedie <jellonator00@gmail.com>
2020-03-09 18:25:19 -0400

    Added pause menu

--- Object/Gui/GuiOverlay.gd
+++ Object/Gui/GuiOverlay.gd
@@ -6,7 +6,21 @@ onready var node_films := $Films
 
 var show_film_timer := 0.0
 
+onready var select_i := 0
+onready var selections := [
+	$Select/Panel/VBox/Continue,
+	$Select/Panel/VBox/Restart,
+	$Select/Panel/VBox/Exit
+]
+
+onready var node_cursor := $Sprite
+
+func update_selection():
+	node_cursor.global_position = selections[select_i].rect_global_position
+
 func _ready():
+	$Select.visible = get_tree().paused
+	$Sprite.visible = get_tree().paused
 	show_film_timer = 1.0
 	yield(get_tree(), "idle_frame")
 	for film in get_tree().get_nodes_in_group("film"):
@@ -33,8 +47,38 @@ func _physics_process(delta):
 		node_films.rect_position.y = clamp(node_films.rect_position.y + delta * 64.0, -16.0, 0.0)
 	else:
 		node_films.rect_position.y = clamp(node_films.rect_position.y - delta * 64.0, -16.0, 0.0)
+	if Input.is_action_just_pressed("action_pause"):
+		get_tree().paused = not get_tree().paused
+		select_i = 0
+		$Select.visible = get_tree().paused
+		$Sprite.visible = get_tree().paused
+		call_deferred("update_selection")
+	if get_tree().paused:
+		if Input.is_action_just_pressed("move_up"):
+			select_i = (select_i - 1) % selections.size()
+			update_selection()
+		if Input.is_action_just_pressed("move_down"):
+			select_i = (select_i + 1) % selections.size()
+			update_selection()
+		if Input.is_action_just_pressed("action_jump"):
+			if select_i == 0:
+				get_tree().paused = false
+			elif select_i == 1:
+				get_tree().paused = false
+				restart_level()
+				return
+			elif select_i == 2:
+				get_tree().paused = false
+				var err = get_tree().change_scene("res://Menu/LevelSelect/LevelSelect.tscn")
+				if err != OK:
+					push_error("Could not load level select [{0}]".format([err]))
+				return
 	if Input.is_action_just_pressed("action_restart"):
+		get_tree().paused = false
 		restart_level()
+		return
+	$Select.visible = get_tree().paused
+	$Sprite.visible = get_tree().paused
 
 func restart_level():
 	var path = get_tree().current_scene.filename

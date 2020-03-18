5e6baa2ac632c3786cff2a911cd2d106c124b58e
Jocelyn Beedie <jellonator00@gmail.com>
2020-03-18 17:28:07 -0400

    Made hitboxes more forgiving

--- Object/EnemyEye/EnemyEye.gd
+++ Object/EnemyEye/EnemyEye.gd
@@ -7,39 +7,54 @@ const REGION_SPEED := 120.0
 
 var num := 0
 
-func _physics_process(delta):
+onready var cast_solid := $SolidCast
+onready var cast_all := $AllCast
+
+func update_sprite(delta: float):
+	var region := $Sprite.region_rect as Rect2
+	region.position.y = fmod(region.position.y - delta * REGION_SPEED, 16.0)
+	var length = cast_solid.global_position.distance_to(cast_solid.get_collision_point())
+	region.size.y = length
+	$Sprite.region_rect = region
+
+func _physics_process(delta: float):
 	if num > 0:
 		t = clamp(t - delta * SPEED, 0.0, 1.0)
 	else:
 		t = clamp(t + delta * SPEED, 0.0, 1.0)
 	$Eye.frame = int(t * 3)
 	if t <= 0.0:
-		if not $RayCast2D.enabled:
-			$RayCast2D.enabled = true
-			$RayCast2D.force_raycast_update()
+		if not cast_solid.enabled:
+			cast_solid.enabled = true
+			cast_solid.force_raycast_update()
+		if not cast_all.enabled:
+			cast_all.enabled = true
+			cast_all.force_raycast_update()
 		$Sprite.frame = 0
 		$Sprite.show()
-		var region := $Sprite.region_rect as Rect2
-		region.position.y = fmod(region.position.y - delta * REGION_SPEED, 16.0)
-		var length = $RayCast2D.global_position.distance_to($RayCast2D.get_collision_point())
-		region.size.y = length
-		$Sprite.region_rect = region
-		var obj = $RayCast2D.get_collider()
-		if obj != null and obj.has_method("do_kill"):
-			obj.do_kill()
+		update_sprite(delta)
+		var obj_solid = cast_solid.get_collider()
+		var obj_all = cast_all.get_collider()
+		var dis = cast_all.get_collision_point().distance_to(cast_solid.get_collision_point())
+#		prints(obj_solid, obj_all, dis, obj_all.has_method("do_kill"))
+		if obj_all != null and obj_all.has_method("do_kill"):
+			if obj_solid == null or dis > 8.0 or obj_all == obj_solid:
+				obj_all.do_kill()
+#		var obj = $RayCast2D.get_collider()
+#		if obj != null and obj.has_method("do_kill"):
+#			var dis = $RayCast2D.get_collision_point().distance_to($RayCast2D.global_position)
+#			if dis >= 8.0:
+#				obj.do_kill()
 	elif t < 1.0:
-		if not $RayCast2D.enabled:
-			$RayCast2D.enabled = true
-			$RayCast2D.force_raycast_update()
+		if not cast_solid.enabled:
+			cast_solid.enabled = true
+			cast_solid.force_raycast_update()
 		$Sprite.show()
 		$Sprite.frame = clamp(int(t*3 + 1), 1, 3)
-		var region := $Sprite.region_rect as Rect2
-		region.position.y = fmod(region.position.y - delta * REGION_SPEED, 16.0)
-		var length = $RayCast2D.global_position.distance_to($RayCast2D.get_collision_point())
-		region.size.y = length
-		$Sprite.region_rect = region
+		update_sprite(delta)
 	else:
-		$RayCast2D.enabled = false
+		cast_solid.enabled = false
+		cast_all.enabled = false
 		$Sprite.hide()
 
 func _on_Area2D_body_entered(_body):
